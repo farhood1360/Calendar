4799d69e8386db7e4648cdd6952528baeb972336
Farhood Rashidi <farhud.rashidi@gmail.com>
2014-12-30 21:06:06 -0600

    Add "primary key" to table

--- model/table.sql
+++ model/table.sql
@@ -1,8 +1,9 @@
 CREATE TABLE 'event' (
+  `id` int(11) NOT NULL,
   `name` varchar(20) NOT NULL,
   `event` varchar(50) NOT NULL,
   `type` varchar(20) NOT NULL,
   `description` varchar(100) NOT NULL,
   `date_picked` datetime NOT NULL,
-  `id` int(11) NOT NULL
+  PRIMARY KEY (`id`)
 );
