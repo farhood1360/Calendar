8fa7a511b8927b191ce7cfc6eec4c3193c017c66
Farhood Rashidi <farhud.rashidi@gmail.com>
2015-01-02 01:46:59 -0600

    Add __construct() and insert() functions.

--- model/class.DATABASE.php
+++ model/class.DATABASE.php
@@ -3,19 +3,44 @@
  * Name: Calendar App/database.php
  * This class configurates the database connection.
  * @author Farhood Rashidi
- * @date 12/28/2014
+ * @date 01/01/2015
  */
 
 class Database {
 
+    //properties
     const DB_HOSTNAME = 'localhost';
-    const DB_USERNAME = 'username';
-    const DB_PASSWORD = 'password';
+    const DB_USERNAME = 'root';
+    const DB_PASSWORD = 'root';
     const DB_NAME = 'calendar';
     private $_db_connect;
+    private $select;
+    private $insert;
     private $_sql;
-    private $_result;
-    private $_name;
+    private $name;
+    private $event;
+    private $type;
+    private $description;
+    private $date;
+    private $time;
+    private $id;
+    
+    //construct function
+    //@param $name
+    //@param $event
+    //@param $type
+    //@param $description
+    //@param $date
+    //@param $time
+    public function __construct($name, $event, $type, $description, $date, $time) {
+        $this->name = $name;
+        $this->event = $event;
+        $this->type = $type;
+        $this->description = $description;
+        $this->date = $date;
+        $this->time = $time;
+        $this->id = rand(100, 200);
+    }
 
     //connection() function
     public function connection() {
@@ -26,16 +51,23 @@ class Database {
     public function select() {
         mysql_select_db(self::DB_NAME) or die(mysql_error());
     }
-    
-    //sql() function
-    public function sql($name){
-        $this->_name = $name;
-        $this->_sql = "SELECT event, description, type, time_picked FROM event WHERE name='{$this->_name}'";
+       
+    //insert() function
+    public function insert(){
+        try{
+            $this->insert = "INSERT INTO event(name, event, type, description, date_picked, time_picked, id) VALUES('{$this->name}', '{$this->event}', '{$this->type}', '{$this->description}', '{$this->date}', '{$this->time}', '{$this->id}')";
+	    $this->_sql = mysql_query($this->insert);
+        } catch (Exception $e) {
+            echo "There is a problem on insertion to database! Please try again. " . $e->getMessage();
+            exit();
+        }
     }
     
     //query() function
     public function query(){
-        $this->_result = mysql_query($this->_sql);
+        $this->select = "SELECT event, description, date_picked, time_picked FROM event WHERE name='{$this->name}'";
+        $this->_sql = mysql_query($this->select);
+        return $this->event . ": ". $this->description .", " . $this->date . "/ " . $this->time;
     }
 
     //destruct() function
