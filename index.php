96442ba936cfef728c0a82b9072cca98602db329
Farhood Rashidi <farhud.rashidi@gmail.com>
2015-01-02 01:08:24 -0600

    Create "database object" and Add an event form.

--- index.php
+++ index.php
@@ -4,10 +4,12 @@
 * This script gets current day, current month, current year, and current timezone of calendar 
 * This script shows the full calendar includes the next and previus buttons. 
 * @author farhoodrashidi
-* @date 12/23/2014
+* @date 01/01/2015
 */
 
 include 'model/class.CALENDAR.php';
+include 'model/database.php';
+
 date_default_timezone_set('CST6CDT');
 if(isset($_GET["day"])){
     $currentDay = $_GET["day"];
@@ -27,7 +29,25 @@ if(isset($_GET["year"])){
     $currentYear = date('Y');
 }
 
+if(isset($_POST["submit"])) {
+    if(empty($_POST['name']) || empty($_POST['event'])) {
+        $warning = "<p class='warning'>You must enter your name and event. <br> Click the submit button again.</p>";
+    } else {
+        $name = addslashes($_POST['name']);
+        $event = addslashes($_POST['event']);
+        $type = addslashes($_POST['type']);
+        $description = addslashes($_POST['des']);
+        $time = addslashes($_POST['time']);
+    }
+}
+
 $newCalendar = new Calendar($currentDay, $currentMonth, $currentYear);
+$newDatabae = new Database($name, $event, $type, $description, $time);
+$newDatabae->connection();
+$newDatabae->select();
+$newDatabae->insert();
+$newDatabae->query();
+$newDatabae->disconnect();
     
 ?>
 
@@ -42,7 +62,46 @@ $newCalendar = new Calendar($currentDay, $currentMonth, $currentYear);
     <body>
         <?=$newCalendar->render(); ?>
         <?php if(isset($_GET["today"])){echo $newCalendar->showMessage();} ?>
-        <br/><br/>
+        <br/><hr/>
+        <form id="eventManager" action="index.php" name="event manager" method="POST">
+            <p>
+                <label for="name"><span>*</span>Your Name</label> 
+                <input name="name" id="name" type="text" size="17" /> 
+            </p>
+            
+            <p>
+                <label for="event"><span>*</span>Your Event</label> 
+                <input name="event" id="event" type="text" size="17" /> 
+            </p>
+
+            <p>
+                <label for="type"><span>*</span>Event Type</label>
+                <select name="type">
+                    <option>Select One</option>
+                    <option>Health</option>
+                    <option>Work</option>
+                    <option>Entertainment</option>
+                    <option>Home</option>
+                </select>
+            </p> 
+            
+            <p>
+                <label for="time"><span>*</span>Event Time</label> 
+                <input name="time" id="time" type="time" size="17" /> 
+            </p>
+            
+            <p>
+                <label for="des"><span>*</span>Event Description</label> 
+                <textarea name="des" id="des" value="Event Description" cols="17"></textarea> 
+            </p>
+
+            <p>
+                <input name="submit" type="submit" id="mySubmit" class="button" value="Submit" />
+                <input name="reset" type="reset" value="Reset" class="button" />
+            </p>
+            
+            <?=$warning; ?>
+        </form>
         </div>
         <footer>
             Copyright <?=date('Y'); ?>, Farhood Rashidi
